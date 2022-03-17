294b0a4bd7e3fe98b12856c287fda6b1b5b6b359
guillen-jorge88 <guillen.jorge88@gmail.com>
2022-03-16 19:30:39 -0500

    ajustes find rows

--- controllers/google/google_sheets_get_by_data_find_value.controllers.js
+++ controllers/google/google_sheets_get_by_data_find_value.controllers.js
@@ -24,78 +24,80 @@ var getByDataFindValue = async (
     if (getRows.data) {
         getRows.data.values.forEach((row, rowIndex) => {
             if (rowIndex >= startRow) {
+                let existElement = [];
                 columnNumber.forEach((colNum, colIndex) => {
-                    if (values[colIndex] != "") {
-                        if (row.length >= colNum) {
-                            row.forEach((element, elemIndex) => {
-                                if (elemIndex === colNum - 1) {
-                                    validateValue(
-                                        element,
-                                        rowList,
-                                        row,
-                                        rowIndex + 1,
-                                        values[colIndex],
-                                        criterion
-                                    );
-                                }
-                            });
-                        }
+                    if (colNum <= row.length) {
+                        values[colIndex] != ""
+                            ? validateValue(
+                                  row[colNum - 1],
+                                  rowList,
+                                  row,
+                                  rowIndex + 1,
+                                  values[colIndex],
+                                  criterion,
+                                  existElement
+                              )
+                            : null;
                     } else {
-                        if (row.length >= colNum) {
-                            row.forEach((element, elemIndex) => {
-                                if (elemIndex === colNum - 1) {
-                                    validateValue(
-                                        element,
-                                        rowList,
-                                        row,
-                                        rowIndex + 1,
-                                        values[colIndex],
-                                        criterion
-                                    );
-                                }
-                            });
-                        }
-                        if (row.length < colNum) {
-                            rowList.push({
-                                rowNumber: rowIndex + 1,
-                                row,
-                            });
-                        }
+                        values[colIndex] != ""
+                            ? existElement.push(false)
+                            : existElement.push(true);
                     }
                 });
+
+                if (existElement.length > 0) {
+                    let countCoincidences = 0;
+                    existElement.forEach((element) => {
+                        element === true ? (countCoincidences += 1) : null;
+                    });
+                    countCoincidences === columnNumber.length
+                        ? rowList.push({
+                              rowNumber: rowIndex + 1,
+                              row,
+                          })
+                        : null;
+                }
             }
         });
         getRows.data.values = rowList;
+        getRows.data.numberRows = rowList.length;
     }
 
     return getRows;
 };
 
-var validateValue = (element, rowList, row, rowNumber, value, criterion) => {
-    let r = {
-        rowNumber,
-        row,
-    };
+var validateValue = (
+    element,
+    rowList,
+    row,
+    rowNumber,
+    value,
+    criterion,
+    existElement
+) => {
     switch (criterion) {
         case "starts with":
             if (element.startsWith(value)) {
-                rowList.push(r);
+                existElement.push(true);
             }
             break;
         case "ends with":
             if (element.endsWith(value)) {
-                rowList.push(r);
+                existElement.push(true);
             }
             break;
         case "includes":
             if (element.includes(value)) {
-                rowList.push(r);
+                existElement.push(true);
             }
             break;
         case "equal":
             if (element === value) {
-                rowList.push(r);
+                existElement.push(true);
             }
+            break;
+        default:
+            existElement.push(false);
             break;
     }
 };
